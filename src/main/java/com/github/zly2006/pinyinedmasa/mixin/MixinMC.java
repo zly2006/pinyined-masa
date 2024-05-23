077f270926a4ce3620b1b981824c6307c7ebc8d5
Liyan Zhao <return65535@qq.com>
2024-05-24 06:21:27 +0800

    0.4

--- src/main/java/com/github/zly2006/pinyinedmasa/mixin/MixinMC.java
+++ src/main/java/com/github/zly2006/pinyinedmasa/mixin/MixinMC.java
@@ -7,7 +7,6 @@ import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,8 +17,6 @@ import java.util.stream.Stream;
 
 @Mixin(MinecraftClient.class)
 public abstract class MixinMC {
-    @Shadow public abstract void loadBlockList();
-
     @Inject(
             method = "method_1485",
             at = @At("HEAD"),
@@ -35,13 +32,13 @@ public abstract class MixinMC {
     }
 
     @Inject(
-            method = "method_43761",
+            method = "method_53862",
             at = @At("HEAD"),
             cancellable = true
     )
     private static void addPinYin4craft(RecipeResultCollection resultCollection, CallbackInfoReturnable<Stream<String>> cir) {
         List<String> list = new ArrayList<>(resultCollection.getAllRecipes().stream()
-                .flatMap(recipe -> recipe.getOutput(resultCollection.getRegistryManager()).getTooltip(null, TooltipContext.Default.BASIC).stream())
+                .flatMap(recipe -> recipe.value().getResult(resultCollection.getRegistryManager()).getTooltip(null, TooltipContext.Default.BASIC).stream())
                 .map(text -> Formatting.strip(text.getString()).trim())
                 .filter(text -> !text.isEmpty()).toList());
         list.addAll(PinyinDict.getFirstAndFullPinyin(String.join(" ", list)));
