170dd5b65960eab0886b38a43f3c9021fc185007
Liyan Zhao <return65535@qq.com>
2024-06-21 00:19:43 +0800

    0.6, mc 1.21

--- src/main/java/com/github/zly2006/pinyinedmasa/mixin/MixinSearchManager.java
+++ src/main/java/com/github/zly2006/pinyinedmasa/mixin/MixinSearchManager.java
@@ -1,10 +1,12 @@
 package com.github.zly2006.pinyinedmasa.mixin;
 
 import com.github.zly2006.pinyinedmasa.PinyinDict;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.recipebook.RecipeResultCollection;
-import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.search.SearchManager;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,15 +17,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-@Mixin(MinecraftClient.class)
-public abstract class MixinMC {
+@Mixin(SearchManager.class)
+public abstract class MixinSearchManager {
     @Inject(
-            method = "method_1485",
+            method = "method_60349",
             at = @At("HEAD"),
             cancellable = true
     )
-    private static void addPinYin4creative(ItemStack stack, CallbackInfoReturnable<Stream<String>> cir) {
-        List<String> list = new ArrayList<>(stack.getTooltip(null, TooltipContext.Default.BASIC.withCreative()).stream()
+    private static void addPinYin4creative(Item.TooltipContext tooltipContext, TooltipType tooltipType, ItemStack stack, CallbackInfoReturnable<Stream<String>> cir) {
+        List<String> list = new ArrayList<>(stack.getTooltip(Item.TooltipContext.DEFAULT, null, TooltipType.Default.BASIC.withCreative()).stream()
                 .map((tooltip) -> Formatting.strip(tooltip.getString()).trim())
                 .filter((string) -> !string.isEmpty())
                 .toList());
@@ -32,13 +34,13 @@ public abstract class MixinMC {
     }
 
     @Inject(
-            method = "method_53862",
+            method = "method_60360",
             at = @At("HEAD"),
             cancellable = true
     )
-    private static void addPinYin4craft(RecipeResultCollection resultCollection, CallbackInfoReturnable<Stream<String>> cir) {
+    private static void addPinYin4craft(DynamicRegistryManager.Immutable immutable, Item.TooltipContext tooltipContext, TooltipType tooltipType, RecipeResultCollection resultCollection, CallbackInfoReturnable<Stream<String>> cir) {
         List<String> list = new ArrayList<>(resultCollection.getAllRecipes().stream()
-                .flatMap(recipe -> recipe.value().getResult(resultCollection.getRegistryManager()).getTooltip(null, TooltipContext.Default.BASIC).stream())
+                .flatMap(recipe -> recipe.value().getResult(resultCollection.getRegistryManager()).getTooltip(Item.TooltipContext.DEFAULT, null, TooltipType.Default.BASIC).stream())
                 .map(text -> Formatting.strip(text.getString()).trim())
                 .filter(text -> !text.isEmpty()).toList());
         list.addAll(PinyinDict.getFirstAndFullPinyin(String.join(" ", list)));
